10549b4e61f2afdb62695a335d617d6ab1eb6be5
anubhav maurya <channelsub47@gmail.com>
2024-11-16 23:41:23 +0530

    my third commit

--- script.js
+++ script.js
@@ -12,3 +12,35 @@ function patternPrintingII(N) {
       console.log(bag)
     }
 }
+
+// find even number in array 
+
+// Example array
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Function to get even numbers from the array
+const getEvenNumbers = (arr) => {
+    return arr.filter(number => number % 2 === 0);
+};
+
+// Call the function and store the result
+const evenNumbers = getEvenNumbers(number);
+
+// Output the result
+console.log("Even numbers in the array:", evenNumbers);
+
+// find odd numbe in the array
+
+// Example array
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Function to get odd numbers from the array
+const getOddNumbers = (arr) => {
+    return arr.filter(number => number % 2 !== 0);
+};
+
+// Call the function and store the result
+const oddNumbers = getOddNumbers(numbers);
+
+// Output the result
+console.log("Odd numbers in the array:", oddNumbers);
